3ebc269c991ef118db4ce0358870d9a0776637e1
Muhammad Hafiz <ibnukamy@gmail.com>
2013-07-27 08:23:07 +0800

    Edited version of form styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, 
+  				  			:first_name, :last_name, :profile_name
+
+  has_many :statuses
+
+  def full_name
+  	first_name + " " + last_name
+  end
 end
